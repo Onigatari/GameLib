c75c827b12baf19e101d8a5a2cd6c4f140b14424
Onigatari <gamosov.gs@yandex.ru>
2023-01-05 23:20:51 +0300

    Version 0.1.0

--- assets/table.js
+++ assets/table.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
     $('#gamesList').DataTable({
+        "language": {
+            "search": "Поиск:",
+            "lengthMenu":     "Кол-во игр на страницы _MENU_",
+            "loadingRecords": "Загрузка...",
+            "processing":     "",
+            "zeroRecords":    "Не найдено совпадающих игр",
+            "paginate": {
+                "first":      "Первый",
+                "last":       "Последний",
+                "next":       "Следующий",
+                "previous":   "Предыдущий"
+            },
+        },
         ordering: false,
         info: false,
         lengthMenu: [
